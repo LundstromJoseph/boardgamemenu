fa49a4a8ca78fde0eb8f86bf0e114774391b7436
Joseph <lundstromjoseph@gmail.com>
2024-10-25 09:21:01 +0200

    fix: 🐛 Button not working

--- src/lib/components/FilterMenu/PlayerCountButton.svelte
+++ src/lib/components/FilterMenu/PlayerCountButton.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import { formatPlayerCount, playerCount } from '../../store/playerCount'
 	import { COLORS } from '../../theme/colors'
-	import RoundButton from '../RoundButton.svelte'
-	import IconText from '../typography/IconText.svelte'
+	import RoundButton from '$lib/components/RoundButton.svelte'
+	import IconText from '$lib/components/typography/IconText.svelte'
 
 	interface Props {
-		index: number;
-		update: (i: number) => void;
+		index: number
+		update: (i: number) => void
 	}
 
-	let { index, update }: Props = $props();
+	let { index, update }: Props = $props()
 </script>
 
 <RoundButton
 	color={COLORS.FILTER.PLAYER_COUNT}
 	selected={$playerCount === index}
-	on:click={() => update(index)}
+	onclick={() => update(index)}
 	label={`Set player count to ${index}`}>
 	<IconText>{formatPlayerCount(index)}</IconText>
 </RoundButton>
